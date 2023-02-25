3b8e7fa770c06ee5dcbcdb17509957d0b044430a
DesignComb <designcomb2021@gmail.com>
2023-02-25 22:05:06 +0800

    test

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -12,7 +12,7 @@ const isSuccess = ref(false)
 
 async function submit() {
   if(isValidHttpUrl(urlObj.origin)){
-    response.data = await $fetch('http://172.17.0.1:8000/urlShorten', {
+    response.data = await $fetch('http://54.249.0.5:8000/urlShorten', {
       method: 'POST',
       body: urlObj,
     })
