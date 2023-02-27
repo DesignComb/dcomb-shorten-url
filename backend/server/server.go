a77f7ad6f20cfdfe9d7666c6bc8298201048ce44
DesignComb <designcomb2021@gmail.com>
2023-02-28 00:00:50 +0800

    test

--- backend/server/server.go
+++ backend/server/server.go
@@ -144,7 +144,7 @@ func SetupAndListen() {
 	router := fiber.New()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins: "http://172.19.0.1, http://54.249.0.5",
+		AllowOrigins: "http://172.19.0.1, http://54.249.0.5, https://dco.tw",
 		AllowHeaders: "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
         AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
