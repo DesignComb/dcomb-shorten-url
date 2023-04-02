ff824350bee4c7f7fc440aea63885d0e11b52ba8
HaoChingSui <hao.sui@kkday.com>
2023-04-02 21:18:52 +0800

    refactor: fiber to gin

--- backend/server/server.go
+++ backend/server/server.go
@@ -3,22 +3,20 @@ package server
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"main/model"
 	"main/utils"
+	"net/http"
 	"strconv"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func redirect(c *fiber.Ctx) error {
-	shortenUrl := c.Params("redirect")
+func redirect(c *gin.Context) {
+	shortenUrl := c.Param("redirect")
 	url, err := model.FindByUrl(shortenUrl)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "could not find url in DB " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "could not find url in DB " + err.Error()})
+		return
 	}
 	// grab any stats you want...
 	url.Clicked += 1
@@ -27,145 +25,136 @@ func redirect(c *fiber.Ctx) error {
 		fmt.Printf("error updating: %v\n", err)
 	}
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map {
-		"origin": url.Origin,
-	})
+	c.JSON(http.StatusOK, gin.H{"origin": url.Origin})
 }
 
-func getAllUrlShorten(c *fiber.Ctx) error {
+func getAllUrlShorten(c *gin.Context) {
 	urls, err := model.GetAllUrlShorten()
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "error getting all goly links " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "error getting all goly links " + err.Error()})
+		return
 	}
-	return c.Status(fiber.StatusOK).JSON(urls)
+	c.JSON(http.StatusOK, gin.H{"data:": urls})
 }
 
-func getUrlShorten(c *fiber.Ctx) error {
-	id, err := strconv.ParseUint( c.Params("id"), 10, 64)
+func getUrlShorten(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "error could not parse id " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "error could not parse id " + err.Error()})
+		return
 	}
 
 	url, err := model.GetUrlShorten(id)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "error could not retrieve url from db " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "error could not retrieve url from db " + err.Error()})
+		return
 	}
-	return c.Status(fiber.StatusOK).JSON(url)
+	c.JSON(http.StatusOK, gin.H{"data:": url})
 }
 
-func createUrlShorten(c *fiber.Ctx) error {
-	c.Accepts("application/json")
+func createUrlShorten(c *gin.Context) {
+	acceptHeader := c.Request.Header.Get("Accept")
+	if acceptHeader != "" && acceptHeader != "application/json" {
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{
+			"error": "Not Acceptable",
+		})
+		return
+	}
 
 	var urlShorten model.UrlShorten
-	err := c.BodyParser(&urlShorten)
+	err := c.ShouldBindJSON(&urlShorten)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "error parsing JSON " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "error parsing JSON " + err.Error()})
+		return
 	}
 
 	if len(urlShorten.Origin) == 0 {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map {
-			"message": "origin column is required",
-		})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "origin column is required"})
+		return
 	}
 
 	if urlShorten.Random {
 		urlShorten.Short = utils.RandomURL(8)
-	}else {
+	} else {
 		if len(urlShorten.Short) == 0 {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map {
-				"message": "short column is required",
-			})
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "short column is required"})
+			return
 		}
 	}
 
 	urlShorten, err = model.CreateUrlShorten(urlShorten)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "could not create urlShorten in db " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "could not create urlShorten in db " + err.Error()})
+		return
 	}
 
-	return c.Status(fiber.StatusOK).JSON(urlShorten)
+	c.JSON(http.StatusOK, gin.H{"data:": urlShorten})
 }
 
-func updateUrlShorten(c *fiber.Ctx) error {
-	c.Accepts("application/json")
+func updateUrlShorten(c *gin.Context) {
+	acceptHeader := c.Request.Header.Get("Accept")
+	if acceptHeader != "" && acceptHeader != "application/json" {
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{
+			"error": "Not Acceptable",
+		})
+		return
+	}
 
 	var urlShorten model.UrlShorten
 
-	err := c.BodyParser(&urlShorten)
+	err := c.ShouldBindJSON(&urlShorten)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "could not parse json " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "could not parse json " + err.Error()})
+		return
 	}
 
 	err = model.UpdateUrlShorten(urlShorten)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "could not update urlShorten link in DB " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "could not update urlShorten link in DB " + err.Error()})
+		return
 	}
 
-	return c.Status(fiber.StatusOK).JSON(urlShorten)
+	c.JSON(http.StatusOK, gin.H{"data:": urlShorten})
 }
 
-func deleteUrlShorten(c *fiber.Ctx) error {
-	id, err := strconv.ParseUint( c.Params("id"), 10, 64)
+func deleteUrlShorten(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "could not parse id from url " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "could not parse id from url " + err.Error()})
+		return
 	}
 
 	err = model.DeleteUrlShorten(id)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
-			"message": "could not delete from db " + err.Error(),
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "could not delete from db " + err.Error()})
+		return
 	}
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map {
-		"message": "urlShorten deleted.",
-	})
+	c.JSON(http.StatusOK, gin.H{"message": "urlShorten deleted."})
 }
-
 
 func SetupAndListen() {
 
-	router := fiber.New()
+	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins: "http://172.19.0.1, http://54.249.0.5, https://dco.tw",
-		AllowHeaders: "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
-        AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowOrigins: []string{"http://172.19.0.1", "http://54.249.0.5", "http://54.249.0.5", "https://dco.tw"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Content-Length", "Accept-Language", "Accept-Encoding", "Connection", "Access-Control-Allow-Origin"},
+		AllowMethods: []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
 	}))
 
-	router.Use(logger.New())
-	router.Use(logger.New(logger.Config{
-		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
-	}))
 
-	router.Get("/api/r/:redirect", redirect)
+	router.GET("/api/r/:redirect", redirect)
 
 	//router.Get("/urlShorten", getAllUrlShorten)
-	router.Get("/api/urlShorten/:id", getUrlShorten)
-	router.Post("/api/urlShorten", createUrlShorten)
+	router.GET("/api/urlShorten/:id", getUrlShorten)
+	router.POST("/api/urlShorten", createUrlShorten)
 	//router.Patch("/urlShorten", updateUrlShorten)
 	//router.Delete("/urlShorten/:id", deleteUrlShorten)
 
-
-	data, _ := json.MarshalIndent(router.Stack(), "", "  ")
+	router.Use(gin.Logger())
+	data, _ := json.MarshalIndent(router.Routes(), "", "  ")
 	fmt.Println(string(data))
 
-	router.Listen(":5000")
-
+	router.Run(":5000")
 }
